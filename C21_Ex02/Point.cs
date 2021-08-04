c248eb25f02cc429042713df719a32127d1d060c
divsros <77684896+divsros@users.noreply.github.com>
2021-08-04 21:42:01 +0300

    Add files via upload

---
+++ C21_Ex02/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C21_Ex02
+{
+    struct Point
+    {
+        private int x;
+        private int y;
+
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+        public void SetPoint(int i_X, int i_Y)
+        {
+            this.x = i_X;
+            this.y = i_Y;
+        }
+        public void AddValues(int i_X, int i_Y)
+        {
+            this.x += i_X;
+            this.y += i_Y;
+        }
+
+
+    }
+}
